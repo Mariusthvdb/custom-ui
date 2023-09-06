bae12a64a2b3d54bb14efbcaced5a61f4a490f6b
Marius <33354141+Mariusthvdb@users.noreply.github.com>
2023-09-06 16:57:26 +0200

    Update update_more_info.js
    to be compatible with 2023.9
    
    thanks to @elchininet from the amazing Kiosk-mode custom card for fixing this in https://github.com/NemesisRE/kiosk-mode/discussions/98#discussioncomment-6888224

--- update_more_info.js
+++ update_more_info.js
@@ -1,5 +1,5 @@
 const Name = "Update more-info";
-const Version = "20230628";
+const Version = "20230906";
 const Description = "standalone";
 const Url = "https://github.com/Mariusthvdb/custom-ui";
 
@@ -63,13 +63,18 @@ function updateMoreInfo(ev) {
       let e;
       for (const node of t) {
         const o = node.getElementsByClassName("key")[0];
-        if (o.innerText.toLowerCase().trim() == "hide attributes") {
-          e = o.parentNode
-            .getElementsByClassName("value")[0]
-            .innerText.split(",")
-            .map((item) => item.replace("_", " ").trim());
-          e.push("hide attributes");
-        }
+        // make compatible for both 2023.8 and 2023.9
+          if (o.innerText.toLowerCase().trim() == "hide attributes") {
+              const valueContainer = o.parentNode.getElementsByClassName("value")[0];
+              const haAttributeValue = valueContainer.querySelector('ha-attribute-value');
+              const text = haAttributeValue
+                  ? haAttributeValue.shadowRoot.textContent
+                  : valueContainer.innerText;
+              e = text
+                  .split(",")
+                  .map((item) => item.replace("_", " ").trim());
+              e.push("hide attributes");
+          }
       }
       for (const node of t) {
         const o = node.getElementsByClassName("key")[0];
